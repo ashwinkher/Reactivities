7847c7c9c969460a8df54aacb451c81f19f72723
Ashwin Kher <ashwinkher@Ashwins-MacBook-Pro.local>
2020-07-15 21:58:19 -0500

    end of section 4

---
+++ Application/Activities/List.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        //step 1:>we created our query,
+        public class Query : IRequest<List<Activity>> { }
+
+        //step 2:>now we need handler
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var activities = await _context.Activities.ToListAsync();
+                return activities;
+            }
+        }
+    }
+}
