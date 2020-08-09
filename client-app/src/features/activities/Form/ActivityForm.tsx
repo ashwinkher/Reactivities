ed2fea6f1a501abb12066d6f3c79a3dac5998d34
Ashwin Kher <ashwinkher@Ashwins-MacBook-Pro.local>
2020-08-09 11:52:33 -0500

    end of section 9

--- client-app/src/features/activities/Form/ActivityForm.tsx
+++ client-app/src/features/activities/Form/ActivityForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, FormEvent, useContext, useEffect } from 'react';
-import { Segment, Form, Button } from 'semantic-ui-react';
+import { Segment, Form, Button, Grid } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import { v4 as uuid } from 'uuid';
 import ActivityStore from '../../../app/stores/activitystore';
@@ -65,7 +65,9 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
   };
 
   return (
-    <Segment clearing>
+    <Grid>
+      <Grid.Column width={10}>
+      <Segment clearing>
       <Form onSubmit={handleSubmit}>
         <Form.Input
           onChange={handleInputChange}
@@ -120,6 +122,10 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
         />
       </Form>
     </Segment>
+      </Grid.Column>
+     
+    </Grid>
+    
   );
 };
 
